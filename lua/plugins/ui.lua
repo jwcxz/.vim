de51f0f55536dde16454889e7563866d28732c1b
Joe Colosimo <jwc@jwcxz.com>
2026-02-02 17:35:14 -0800

    Disable lazy-loading for telescope

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -40,6 +40,7 @@ return {
 
     {
         'nvim-telescope/telescope.nvim', version = '*',
+        lazy = false,
         dependencies = {
             'nvim-lua/plenary.nvim',
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
