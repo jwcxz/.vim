8ab28fe8b48eeb9b4be94b9fce162615e7b4c81d
Joe Colosimo <jwc@jwcxz.com>
2026-02-06 09:50:54 -0800

    add gitsigns and update packages

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -17,4 +17,20 @@ return {
 
     { 'tmhedberg/SimpylFold' },
 
+    {
+        'lewis6991/gitsigns.nvim',
+        lazy = false,
+        opts = {
+            signcolumn = false,
+            numhl = true,
+            --sign_priority = 15, -- higher than diagnostic,todo signs. lower than dapui breakpoint sign
+            on_attach = function(buffer)
+                local gs = package.loaded.gitsigns
+                vim.keymap.set({ 'n', 'v' },
+                    '<leader>gl', gs.toggle_linehl,
+                    { buffer = buffer, silent = true, desc = "Toggle Gitsigns line highlight" })
+                end
+        },
+    }
+
 }
