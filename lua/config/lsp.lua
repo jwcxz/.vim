115e15381c430858f59a552e5c6f5b9ca4c90fd4
Joe Colosimo <jwc@jwcxz.com>
2026-04-06 13:44:48 -0700

    add marksman markdown LSP server

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -7,6 +7,7 @@ enabled_lsp_servers = {
     'ts_ls',        -- npm install -g typescript-language-server typescript
     'lua_ls',       -- brew install lua-language-server
     'texlab',       -- cargo install --git https://github.com/latex-lsp/texlab
+    'marksman',     -- brew install marksman
 }
 
 for _, srv in ipairs(enabled_lsp_servers) do
